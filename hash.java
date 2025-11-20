7d295ad90671f174ff8336018c6d52f8b0690582
DraxTokilled <sandroapolinar80@gmail.com>
2025-11-20 00:59:25 -0600

    Subida del proyecto Hash

--- hash.java
+++ hash.java
@@ -3,24 +3,42 @@ import java.util.*;
 
 public class hash {
 
+    static final int TAM = 10;
+    static List<Integer>[] tabla = new ArrayList[TAM];
+
+    public static int hash(int x) {
+        return x % TAM;
+    }
+
+    public static void inicializar() {
+        for (int i = 0; i < TAM; i++) {
+            tabla[i] = new ArrayList<>();
+        }
+    }
+
+    public static void insertar(int x) {
+        int pos = hash(x);
+        tabla[pos].add(x);
+    }
+
     public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new FileReader("entrada.txt"));
-        HashMap<Integer, String> tabla = new HashMap<>();
+        inicializar();
 
+        BufferedReader br = new BufferedReader(new FileReader("entrada_hash.txt"));
         String linea;
         while ((linea = br.readLine()) != null) {
-            String[] p = linea.split(",");
-            tabla.put(Integer.parseInt(p[0]), p[1]);
+            insertar(Integer.parseInt(linea.trim()));
         }
         br.close();
 
-        PrintWriter pw = new PrintWriter("salida.txt");
-        for (int k : tabla.keySet())
-            pw.println(k + " -> " + tabla.get(k));
+        PrintWriter pw = new PrintWriter("salida_hash.txt");
+        pw.println("=== TABLA HASH ===\n");
+        for (int i = 0; i < TAM; i++) {
+            pw.println(i + " -> " + tabla[i]);
+        }
         pw.close();
 
-        System.out.println("HashTable generada.");
+        System.out.println("Hash completo. Revisa salida_hash.txt");
     }
 }
-
